1bba88af6926d45d70a7c8c7f15c5468f6e1f22e
Nickflanagn24 <lpao26ad@students.codeinstitute.net>
2025-03-16 22:52:03 +0000

    Added custom decorator for admin-only access

---
+++ bookings/decorators.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponseForbidden
+from django.shortcuts import redirect
+
+def admin_required(view_func):
+    """Decorator to restrict access to admin users only."""
+    def wrapper(request, *args, **kwargs):
+        if request.user.is_authenticated and request.user.role == "admin":
+            return view_func(request, *args, **kwargs)
+        return HttpResponseForbidden("You do not have permission to view this page.")
+    return wrapper
