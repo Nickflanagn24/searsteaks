344ba724460adc9143d51cf83b1b2d7805e2a0d2
Nickflanagn24 <lpao26ad@students.codeinstitute.net>
2025-03-16 16:43:49 +0000

    Created custom user registration form

---
+++ bookings/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    phone_number = forms.CharField(max_length=15, required=True)
+    role = forms.ChoiceField(choices=CustomUser.ROLE_CHOICES)
+
+    class Meta:
+        model = CustomUser
+        fields = ["username", "email", "phone_number", "role", "password1", "password2"]
