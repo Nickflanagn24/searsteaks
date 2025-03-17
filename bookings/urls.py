f67fadd8c7c3a432ec498be46c0bd0faa5dd18bf
Nickflanagn24 <lpao26ad@students.codeinstitute.net>
2025-03-17 20:21:23 +0000

    Added URL for floor plan selection page

--- bookings/urls.py
+++ bookings/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("login/", views.user_login, name="login"),
     path("logout/", views.user_logout, name="logout"),
     path("admin-dashboard/", views.admin_dashboard, name="admin_dashboard"),
+    path("make-booking/", views.make_booking, name="make_booking"),
+    path("my-bookings/", views.my_bookings, name="my_bookings"),
+    path("floor-plan/", views.floor_plan, name="floor_plan"),
 ]
