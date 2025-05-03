caea26acf60e010d87e0f82921413b24d968f1ce
Nickflanagn24 <lpao26ad@students.codeinstitute.net>
2025-05-03 13:36:25 +0000

    Replace inline hero image styles with CSS classes for better caching control

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Project-level static files
+    os.path.join(BASE_DIR, 'bookings/static'),  # Project-level static files
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
